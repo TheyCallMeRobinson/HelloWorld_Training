9f8770311eb05810977f8991cd5ddd6580fc8468
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2023-12-21 20:06:09 +0300

    Print Hello World!

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter x value: ");
-        double x = scanner.nextDouble();
-        System.out.println("Please enter y value: ");
-        double y = scanner.nextDouble();
-        if (y == 5.0 / 2.0 * x + 8.5 || y == -2 || x == Math.pow(y - 2, 2) + 2) {
-            System.out.println("Blue");
-        } else if (x > Math.pow(y - 2, 2) + 2) {
-            System.out.println("Orange");
-        } else if (y > 5.0 / 2.0 * x + 8.5 && y < -2) {
-            System.out.println("Green");
-        } else if (y > -2) {
-            System.out.println("Gray");
-        } else {
-            System.out.println("Yellow");
-        }
+        System.out.println("Hello World!");
     }
 }
