b21edc8703fcb17a440d5cfa35b3af7023bdb18e
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2023-12-21 20:16:34 +0300

    Add 1 to 10 for loop printer

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
