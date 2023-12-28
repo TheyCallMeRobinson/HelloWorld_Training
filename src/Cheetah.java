f7cbf408945eeb730ebdcf0d731fd8581a032650
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2023-12-28 21:08:10 +0300

    Override getName methods in Dog and Cheetah

--- src/Cheetah.java
+++ src/Cheetah.java
@@ -5,4 +5,9 @@ public class Cheetah extends Animal {
     public Cheetah(String name) {
         super(name, "Spotted-red", 2);
     }
+
+    @Override
+    public String getName() {
+        return "This is a cheetah named " + name;
+    }
 }
--- src/Dog.java
+++ src/Dog.java
@@ -5,4 +5,9 @@ public class Dog extends Animal {
     public Dog(String name) {
         super(name, "Brown", 2);
     }
+
+    @Override
+    public String getName() {
+        return "This is a dog named " + name;
+    }
 }
