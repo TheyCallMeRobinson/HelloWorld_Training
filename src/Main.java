92a2716205e74e9530c082d851f2ad63abab3d36
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2023-12-28 19:10:50 +0300

    Delete redundant code

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
-        System.out.println("World is beautiful!");
-        System.out.println("World is even more beautiful");
 
-        int a = 10 + 80;
-        int c = 50;
-        int b = -10 + 30 - c;
     }
 }
